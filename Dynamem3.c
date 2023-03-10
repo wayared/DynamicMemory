337e02f60fa948470545af7aed38c5c6c07fe8a1
wayared <jarecast@espol.edu.ec>
2023-03-10 15:54:49 -0800

    how to use malloc

--- Dynamem3.c
+++ Dynamem3.c
@@ -4,3 +4,15 @@
 //
 // void *p = malloc (10*sizeof(int)) 10 is the number of the elemts 
 //
+int main(){
+
+int *p = (int *)malloc(3 * sizeof(int))
+
+	printf(p);
+	
+	*p = 2;
+	*(p+1) = 4;
+	*(p+2) = 6;
+	printf("%d", *(p+1));
+
+}
