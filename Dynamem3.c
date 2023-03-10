dd43c406e6a427e43b0699067e75ac3e53a35714
wayared <jarecast@espol.edu.ec>
2023-03-10 15:48:01 -0800

    how to use malloc

---
+++ Dynamem3.c
@@ -0,0 +1,6 @@
+// Dynamic memory - how to use malloc
+// void *p = mallloc (sizeof(int)) sizeof is the size of one unit 
+// print p print the last dir address reserved according the element type 
+//
+// void *p = malloc (10*sizeof(int)) 10 is the number of the elemts 
+//
