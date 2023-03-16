7b5caaf50fc229293c0ae0191cad680ff09262cf
wayared <jarecast@espol.edu.ec>
2023-03-15 22:29:55 -0700

    Pointers as function return

--- Dynamem5.c
+++ Dynamem5.c
@@ -6,17 +6,18 @@ void PrintHelloWorld(){
 	printf("Hello World\n");
 }
 
-int *Add(int* a, int* b){
+int *Add(int* a, int* b){ //Called function - returns pointer to integer
 
+	//int c = (*a) + (*b);
 	int c = (*a) + (*b);
 	return &c;
 }
 
-int main(){
+int main(){ // Calling function
 
 	int a = 2, b = 4;
 	int* ptr = Add(&a,&b);
-	PrintHelloWorld();
+	//PrintHelloWorld();
 	printf("Sum = %d\n", *ptr);
 		
 }
