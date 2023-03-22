2800052e12e20072eef399fca87f32a7fe3786df
wayared <jarecast@espol.edu.ec>
2023-03-21 21:43:31 -0700

    Showing how dynamic memory works

--- Dynamem9.c
+++ Dynamem9.c
@@ -22,5 +22,18 @@ void Play(int bet){
 	else{
 	      cash -= bet;
       	      printf("You Loose! Result = \"%c %c %c\" Total Cash = %d\n", C[0],C[1],C[2]); 			      
+}
+
+int main() {
+	int bet;
+	printf("Welcome to the Virtual Casino\n");
+	printf("Total cash = $%d\n",cash);
+	while(cash > 0){
+	  	printf("What's your bet? $");
+		scanf("%n",&bet);
+		if(bet == 0 || bet > cash) break;
+		Play(bet);
+
+	}
 
 }
